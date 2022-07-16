6642dde8147b08adcc62c36abdc36c78077d3fb2
H-NGT <nov.13.v3.0@gmail.com>
2022-07-17 00:34:43 +0900

    Initial Commit

---
+++ GitHubTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubTest
+//
+//  Created by 永田浩之 on 2022/07/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
